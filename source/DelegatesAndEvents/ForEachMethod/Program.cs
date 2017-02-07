fc3215ff40c8edb94ee8d9bb7e92a03ba1a8d598
Gyuwon <gyuwon@live.com>
2017-02-08 01:01:35 +0900

    대리자 예제를 추가합니다.

---
+++ source/DelegatesAndEvents/ForEachMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEachMethod
+{
+    class Program
+    {
+        public delegate void IntAction(int value);
+
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            IntAction action = Console.WriteLine;
+
+            int sum = 0;
+            action += n => sum += n;
+
+            ForEach(array, action);
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        public static void ForEach(IEnumerable<int> values, IntAction action)
+        {
+            foreach (int value in values)
+            {
+                action.Invoke(value);
+            }
+        }
+    }
+}
